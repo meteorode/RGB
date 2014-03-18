3a0eabfca280da1f233c147136266fba94d0a2fe
yangfannet <yangfan@gmail.com>
2014-03-18 19:43:59 +0800

    bug fix

--- js/html_actuator.js
+++ js/html_actuator.js
@@ -25,10 +25,10 @@ HTMLActuator.prototype.actuate = function (grid, metadata) {
     self.updateBestScore(metadata.bestScore);
 
     if (metadata.terminated) {
-      if (metadata.won && !metadata.keepPlaying) {
-        self.message(true); // You win
-      } else if (metadata.over) {
+      if (metadata.over) {
         self.message(false); // You lose
+      } else if (metadata.won && !metadata.keepPlaying) {
+        self.message(true); // You win
       }
     }
 
